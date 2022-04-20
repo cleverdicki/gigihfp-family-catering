da8895ce812aef693b855e1d35fc0642041981f8
dicki <dickiclever@gmail.com>
2022-04-20 13:02:08 +0700

    modify dependent delete between menu and menu_category model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,5 @@
 class Menu < ApplicationRecord
-  has_many :menu_categories
+  has_many :menu_categories, dependent: :delete_all
   has_many :order_menus
   has_many :categories, through: :menu_categories
   has_many :orders, through: :order_menus
